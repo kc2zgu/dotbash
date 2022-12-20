a032d616bcbc8d9f784818af34e5b84f41fc686b
Stephen Cavilia <stephen.cavilia@redcom.com>
2022-12-20 09:32:25 -0500

    check if local::lib is installed if ~/perl5 exists

--- env-20-local-lib.sh
+++ env-20-local-lib.sh
@@ -1,6 +1,10 @@
 if [ -d ~/perl5 ]; then
-    LL_VARS=$(perl -Mlocal::lib)
-    eval "$LL_VARS"
+    LL_VARS=$(perl -Mlocal::lib 2>/dev/null)
+    if [ $? -eq 0 ]; then
+        eval "$LL_VARS"
+    else
+        ewarn "~/perl5 found but local::lib not installed"
+    fi
 
     unset LL_VARS
 fi
